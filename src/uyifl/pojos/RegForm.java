c1447e1d1ce06f5ec4f76da9f49e8c384aa7a7eb
dan <dan@localhost>
2010-10-21 10:46:27 -0600

    registration servlet now showing paramaters passed to it

--- src/uyifl/pojos/RegForm.java
+++ src/uyifl/pojos/RegForm.java
@@ -1,5 +1,46 @@
 package uyifl.pojos;
 
+import com.google.appengine.api.datastore.PhoneNumber;
+import com.google.appengine.api.datastore.PostalAddress;
+
 public class RegForm {
 
+	private String firstName;
+	private String lastName;
+	private PostalAddress address;
+	private PhoneNumber homeNumber;
+	private PhoneNumber cellNumber;
+	
 }
+
+/*
+ * The datastore supports the following core value types:
+ * 
+ * java.lang.String
+ * com.google.appengine.api.datastore.ShortBlob
+ * boolean
+ * java.lang.Boolean
+ * short
+ * java.lang.Short
+ * int
+ * java.lang.Integer
+ * long
+ * java.lang.Long
+ * float
+ * java.lang.Float
+ * double
+ * java.lang.Double
+ * java.util.Date
+ * com.google.appengine.api.users.User
+ * com.google.appengine.api.datastore.Text
+ * com.google.appengine.api.datastore.Blob
+ * com.google.appengine.api.datastore.Key
+ * com.google.appengine.api.datastore.Category
+ * com.google.appengine.api.datastore.Email
+ * com.google.appengine.api.datastore.GeoPt
+ * com.google.appengine.api.datastore.IMHandle
+ * com.google.appengine.api.datastore.Link
+ * com.google.appengine.api.datastore.PhoneNumber
+ * com.google.appengine.api.datastore.PostalAddress
+ * com.google.appengine.api.datastore.Rating
+ */
--- src/uyifl/servlets/Registration.java
+++ src/uyifl/servlets/Registration.java
@@ -1,15 +1,52 @@
 package uyifl.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class Registration extends HttpServlet {
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("Hello, servlet<br>POST<hr>");
+
+		displayParams(req, out);
+	}
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("Hello, servlet<br>GET<hr>");
+
+		displayParams(req, out);
+	}
+
+	private void displayParams(HttpServletRequest req, PrintWriter out) {
+		// just display all the parameters received
+		Enumeration<String> parameterNames = req.getParameterNames();
+		while (parameterNames.hasMoreElements()) {
+			String paramName = parameterNames.nextElement();
+			out.println("<br><B>" + paramName + "</B>");
+			String[] paramValues = req.getParameterValues(paramName);
+			if (paramValues.length == 1) {
+				String paramValue = paramValues[0];
+				if (paramValue.length() == 0)
+					out.print("=<I>No Value</I>");
+				else
+					out.print("=" + paramValue);
+			} else {
+				out.println("<UL>");
+				for (int i = 0; i < paramValues.length; i++) {
+					out.println("<LI>" + paramValues[i]);
+				}
+				out.println("</UL>");
+			}
+		}
 	}
 }
